2e16193f58d7f630203a47482bfc8b5394ceb3ce
pietari pakarinen <pietari.pakarinen@eficode.com>
2020-04-15 14:36:07 +0300

    1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -12,39 +12,41 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content 
-      part1={part1} 
-      exercises1={exercises1}
-      part2={part2}
-      exercises2={exercises2}
-      part3={part3}
-      exercises3={exercises3}/>
-      <Total 
-      exercises1={exercises1}
-      exercises2={exercises2}
-      exercises3={exercises3}/>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3} />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3} />
     </div>
   )
 }
 
 const Header = (props) => {
-  return (<h1>{props.course}</h1>) 
+  return (<h1>{props.course}</h1>)
 }
 
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
   )
 }
 
