771fbd452b1a87d6ece362c2ed9ac048021fc2d7
Pietari pakarinen <pietari.pakarinen@tuni.fi>
2023-06-03 14:39:18 +0300

    add exercise 1.5 solution

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+
+  }
 
   return (
     <>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
@@ -48,9 +51,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <>
-      <Part part={props.parts[0]} exercises={props.exercises}/>
-      <Part part={props.parts[1]} exercises={props.exercises}/>
-      <Part part={props.parts[2]} exercises={props.exercises}/>
+      <Part part={props.parts[0]} exercises={props.exercises} />
+      <Part part={props.parts[1]} exercises={props.exercises} />
+      <Part part={props.parts[2]} exercises={props.exercises} />
     </>
   )
 }
