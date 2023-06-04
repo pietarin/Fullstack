51ab714e2fc2e91ccfc8f078bd3d28ec4b564998
Pietari pakarinen <pietari.pakarinen@tuni.fi>
2023-06-04 14:44:00 +0300

    add solution to 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,32 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>
+        Good {props.good}
+      </p>
+      <p>
+        Neutral {props.neutral}
+      </p>
+      <p>
+        Bad {props.bad}
+      </p>
+      <p>
+        All {props.total}
+      </p>
+      <p>
+        Average {props.average}
+      </p>
+      <p>
+        Positive {props.positive}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -47,25 +73,7 @@ const App = () => {
       <Button handleClick={() => handleGoodClick()} text="Good" />
       <Button handleClick={() => handleNeutralClick()} text="Neutral" />
       <Button handleClick={() => handleBadClick()} text="Bad" />
-      <h2>Statistics</h2>
-      <p>
-        Good {good}
-      </p>
-      <p>
-        Neutral {neutral}
-      </p>
-      <p>
-        Bad {bad}
-      </p>
-      <p>
-        All {total}
-      </p>
-      <p>
-        Average {average}
-      </p>
-      <p>
-        Positive {positive}
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
     </div>
   )
 }
