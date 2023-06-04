5c2b71c4ae17a479af69f7a81c15d854163ac402
Pietari pakarinen <pietari.pakarinen@tuni.fi>
2023-06-04 14:50:13 +0300

    add solution to 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,29 +7,40 @@ const Button = (props) => (
 )
 
 const Statistics = (props) => {
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <p>
-        Good {props.good}
-      </p>
-      <p>
-        Neutral {props.neutral}
-      </p>
-      <p>
-        Bad {props.bad}
-      </p>
-      <p>
-        All {props.total}
-      </p>
-      <p>
-        Average {props.average}
-      </p>
-      <p>
-        Positive {props.positive}
-      </p>
-    </div>
-  )
+  if (props.total > 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>
+          Good {props.good}
+        </p>
+        <p>
+          Neutral {props.neutral}
+        </p>
+        <p>
+          Bad {props.bad}
+        </p>
+        <p>
+          All {props.total}
+        </p>
+        <p>
+          Average {props.average}
+        </p>
+        <p>
+          Positive {props.positive}
+        </p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>
+          No feedback given
+        </p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
