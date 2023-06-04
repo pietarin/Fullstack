ec5b18a3891a8d01c184eea1cdc38844c16f7667
Pietari pakarinen <pietari.pakarinen@tuni.fi>
2023-06-04 15:29:49 +0300

    add solution to 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,28 +6,34 @@ const Button = (props) => (
   </button>
 )
 
+const StatisticLine = (props) => (
+  <>
+    {props.text} {props.value}
+  </>
+)
+
 const Statistics = (props) => {
   if (props.total > 0) {
     return (
       <div>
         <h2>Statistics</h2>
         <p>
-          Good {props.good}
+          <StatisticLine text="Good" value={props.good} />
         </p>
         <p>
-          Neutral {props.neutral}
+          <StatisticLine text="Neutral" value={props.neutral} />
         </p>
         <p>
-          Bad {props.bad}
+          <StatisticLine text="Bad" value={props.bad} />
         </p>
         <p>
-          All {props.total}
+          <StatisticLine text="All" value={props.total} />
         </p>
         <p>
-          Average {props.average}
+          <StatisticLine text="Average" value={props.average} />
         </p>
         <p>
-          Positive {props.positive}
+          <StatisticLine text="Positive" value={props.positive} />
         </p>
       </div>
     )
