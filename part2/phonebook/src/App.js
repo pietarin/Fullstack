e33667f47b9ca0cd222038e7a7ef649731b7ddc1
Pietari pakarinen <pietari.pakarinen@tuni.fi>
2023-09-02 13:37:44 +0300

    add solution to 3.19, 3.20 and 3.21

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -42,6 +42,12 @@ const App = () => {
         setTimeout(() => {
           setMessage(null)
         }, 5000)
+      }).catch(error => {
+        setErrorMessage(error.response.data.error)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        console.log(error)
       })
     } else {
       if (window.confirm(`'${newName}' is already added to phonebook, replace the old number with a new one?`)) {
